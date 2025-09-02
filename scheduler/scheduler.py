c9c87bee6941556e188d72a38da61da0e03790b4
KawaiiZT <Trisit.93@gmail.com>
2025-09-02 11:51:49 +0700

    finished part 9 chapter 3

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,5 +1,5 @@
-import time, pika
-
+import time
+import os
 from bson import json_util
 from producer import produce
 from database import get_router_info
@@ -7,21 +7,21 @@ from database import get_router_info
 
 def scheduler():
 
-    INTERVAL = 10.0
+    INTERVAL = 60.0
     next_run = time.monotonic()
     count = 0
 
     while True:
         now = time.time()
         now_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(now))
-        ms = int((now % 1) * 1000)  
+        ms = int((now % 1) * 1000)
         now_str_with_ms = f"{now_str}.{ms:03d}"
         print(f"[{now_str_with_ms}] run #{count}")
 
         try:
             for data in get_router_info():
                 body_bytes = json_util.dumps(data).encode("utf-8")
-                produce("localhost", body_bytes)
+                produce(os.environ.get("RABBITMQ"), body_bytes)
         except Exception as e:
             print(e)
             time.sleep(3)
@@ -29,5 +29,6 @@ def scheduler():
         next_run += INTERVAL
         time.sleep(max(0.0, next_run - time.monotonic()))
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     scheduler()
