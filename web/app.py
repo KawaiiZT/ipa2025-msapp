2a2c199a4df6f84d2c15e9304e32e8fb30c16d28
KawaiiZT <Trisit.93@gmail.com>
2025-09-07 15:06:02 +0700

    edit blackline

--- web/app.py
+++ web/app.py
@@ -27,7 +27,8 @@ def add_router():
     password = request.form.get("password")
 
     if ip and username and password:
-        routers.insert_one({"ip": ip, "username": username, "password": password})
+        routers.insert_one({"ip": ip, "username": username, 
+        "password": password})
     return redirect("/")
 
 
@@ -39,7 +40,8 @@ def delete_router(id):
 
 @app.route("/router/<ip>", methods=["GET"])
 def router_detail(ip):
-    docs = db.interface_status.find({"router_ip": ip}).sort("timestamp", -1).limit(3)
+    docs = db.interface_status.find({"router_ip": ip})
+    .sort("timestamp", -1).limit(3)
 
     return render_template(
         "router_detail.html",
